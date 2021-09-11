ab65461b712f29f9c10ad9289c32a5d422e80e06
hmadry <hassanmadry89@gmail.com>
2021-09-10 21:10:25 -0400

    first commit

---
+++ js/social.js
@@ -0,0 +1,20 @@
+<script type="text/javascript">
+      VanillaTilt.init(document.querySelectorAll(".sci li a"), {
+        max: 30,
+        speed: 400,
+        glare: true,
+        "max-glare": 1,
+      });
+
+      let list = document.querySelectorAll(".sci li");
+      let bg = document.querySelector(".bg");
+      list.forEach((elements) => {
+        elements.addEventListener("mouseenter", function (event) {
+          let color = event.target.getAttribute("data-color");
+          bg.style.backgroundColor = color;
+        });
+        elements.addEventListener("mouseleave", function (event) {
+          bg.style.backgroundColor = "#fff";
+        });
+      });
+    </script>
